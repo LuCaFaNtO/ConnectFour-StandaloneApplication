6359cb55821fef26b50e82bb5f7f279cdfaed056
luca.fanto <luca.fanto@supsi.ch>
2024-05-14 18:02:02 +0200

    feature-aboutView: new plugin for version and artifactId

--- frontend/src/main/java/ch/supsi/connectfour/frontend/view/AboutView.java
+++ frontend/src/main/java/ch/supsi/connectfour/frontend/view/AboutView.java
@@ -1,6 +1,7 @@
 package ch.supsi.connectfour.frontend.view;
 
 import ch.supsi.connectfour.frontend.model.edit.language.UpdateLanguageInterface;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,8 +11,11 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,8 +30,19 @@ public class AboutView implements Initializable, UpdateLanguageInterface {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         fxmlLoaderAboutView = new FXMLLoader(getClass().getResource(fxmlLocation), resourceBundle);
-        version.setText(getClass().getPackage().getImplementationVersion());
-        artifactId.setText(getClass().getPackage().getName().split("\\.")[3]);
+
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model;
+            if ((new File("frontend/pom.xml")).exists())
+                model = reader.read(new FileReader("frontend/pom.xml"));
+            else
+                model = reader.read(new InputStreamReader(getClass().getResourceAsStream("/META-INF/maven/ch.supsi.connectfour/frontend/pom.xml")));
+            version.setText(model.getVersion());
+            artifactId.setText(model.getArtifactId());
+        } catch (IOException | XmlPullParserException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
