8957d56a852dfa596c87b724319b6585a743abf2
luca.fanto <luca.fanto@supsi.ch>
2024-05-28 18:15:05 +0200

    fix/designPatter: infoBar

--- frontend/src/main/java/ch/supsi/connectfour/frontend/controller/InfoBarController.java
+++ frontend/src/main/java/ch/supsi/connectfour/frontend/controller/InfoBarController.java
@@ -1,14 +1,23 @@
 package ch.supsi.connectfour.frontend.controller;
 
-import ch.supsi.connectfour.frontend.model.InfoBarModel;
+import ch.supsi.connectfour.backend.application.ObserverControllerInterface;
+import ch.supsi.connectfour.backend.application.observer.FinishGameObserver;
+import ch.supsi.connectfour.backend.application.observer.ObserverController;
+import ch.supsi.connectfour.backend.application.observer.SavingGameObserver;
+import ch.supsi.connectfour.backend.application.observer.TurnChangeObserverInterface;
 import ch.supsi.connectfour.frontend.view.InfoBarView;
 
-public class InfoBarController implements InfoBarControllerInterface{
+public class InfoBarController implements InfoBarControllerInterface, FinishGameObserver, TurnChangeObserverInterface, SavingGameObserver {
     private static InfoBarController instance = null;
-    private final InfoBarModelInterface infoBarModel;
+    private InfoBarView infoBar;
+    private final ObserverControllerInterface observerController;
 
     private InfoBarController() {
-        this.infoBarModel = InfoBarModel.getInstance();
+        this.observerController = ObserverController.getInstance();
+
+        this.observerController.registerFinishGameObserver(this);
+        this.observerController.registerTurnChangeObserver(this);
+        this.observerController.registerSavingGameObserver(this);
     }
 
     public static InfoBarController getInstance() {
@@ -17,6 +26,26 @@ public class InfoBarController implements InfoBarControllerInterface{
 
     @Override
     public void addInfoBar(InfoBarView infoBar) {
-        infoBarModel.addInfoBar(infoBar);
+        this.infoBar = infoBar;
+    }
+    @Override
+    public void win(String playerName, String playerSymbol) {
+        infoBar.win(playerName, playerSymbol);
+    }
+
+    @Override
+    public void gridFull() {
+        infoBar.gridFull();
+    }
+
+
+    @Override
+    public void changeTurn(String playerName, String playerSymbol) {
+        infoBar.showTurn(playerName, playerSymbol);
+    }
+
+    @Override
+    public void onGameSaved() {
+        infoBar.showSaveGame();
     }
 }
--- frontend/src/main/java/ch/supsi/connectfour/frontend/controller/InfoBarModelInterface.java
+++
@@ -1,7 +0,0 @@
-package ch.supsi.connectfour.frontend.controller;
-
-import ch.supsi.connectfour.frontend.view.InfoBarView;
-
-public interface InfoBarModelInterface {
-    void addInfoBar(InfoBarView infoBar);
-}
